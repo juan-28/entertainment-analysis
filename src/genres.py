77e9562528162ef6618bb02f31b57aaeab470080
Surya Pranav Sukumaran <ps2pranav@gmail.com>
2024-02-29 15:56:29 -0500

    got final merged data
    Former-commit-id: caed13f45f39568de7ab14cb738182627d839d7a

--- src/genres.py
+++ src/genres.py
@@ -1,11 +1,60 @@
 import pandas as pd
+import time
 
-# loading imdb data
-
-data_rating = pd.read_csv(
-    "/Users/pranavsukumaran/Desktop/netflix/project/data/data-2.tsv", sep="\t"
+### File Paths ####
+filepath1 = (
+    "/Users/pranavsukumaran/Desktop/Personal_dev/netflix/project/data/data-2.tsv"
 )
-
-data_basic = pd.read_csv(
-    "/Users/pranavsukumaran/Desktop/netflix/project/data/data-3.tsv", sep="\t"
+filepath2 = (
+    "/Users/pranavsukumaran/Desktop/Personal_dev/netflix/project/data/data-3.tsv"
 )
+netflix_path = "/Users/pranavsukumaran/Desktop/Personal_dev/netflix/project/data/processed_data.csv"
+output_path = "/Users/pranavsukumaran/Desktop/Personal_dev/netflix/project/data/"
+# ----------------------------------------------------------------------------------------#
+
+
+# Function to load data
+def load_imdb(filepath1, filepath2, netflix_path):
+
+    data_rating = pd.read_csv(filepath1, sep="\t")
+    data_basic = pd.read_csv(filepath2, sep="\t")
+    netflix_data = pd.read_csv(netflix_path)
+    return data_rating, data_basic, netflix_data
+
+
+def merge_and_save(data_basic, data_rating, netflix_data, output_path):
+    final_data = data_basic[
+        [
+            "tconst",
+            "titleType",
+            "primaryTitle",
+            "isAdult",
+            "startYear",
+            "runtimeMinutes",
+            "genres",
+        ]
+    ]
+    merged_data = pd.merge(final_data, data_rating, on="tconst")
+    merged_data["Title_lower"] = merged_data["primaryTitle"].str.lower()
+    netflix_data["Title_lower"] = netflix_data["Title"].str.lower()
+    imdb_max_votes = merged_data.loc[
+        merged_data.groupby("Title_lower")["numVotes"].idxmax()
+    ]
+    final_merge = imdb_max_votes.merge(netflix_data, on="Title_lower", how="inner")
+    final_merge = final_merge[
+        ~final_merge["titleType"].isin(["tvMovie", "video", "videoGame"])
+    ]
+    final_merge = final_merge.drop(
+        ["tconst", "isAdult", "runtimeMinutes", "Title_lower", "Title", "Type"], axis=1
+    )
+    final_merge.to_csv(f"{output_path}final_merged_data.csv", index=False)
+
+
+# execution
+
+start_time = time.time()
+data_rating, data_basic, netflix_data = load_imdb(filepath1, filepath2, netflix_path)
+final_merge = merge_and_save(data_basic, data_rating, netflix_data, output_path)
+end_time = time.time()
+total_time = end_time - start_time
+print(f"Processing time using Pandas: {total_time:.2f} seconds")
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 
+
 def load_and_filter_data(data_path, selected_profiles):
     """
     Loads the CSV data, filters by selected profiles, and converts the 'Duration' column.
@@ -14,9 +15,10 @@ def load_and_filter_data(data_path, selected_profiles):
     """
 
     df = pd.read_csv(data_path)
-    filtered_data = df[df['Profile Name'].isin(selected_profiles)]
-    filtered_data['Duration'] = pd.to_timedelta(filtered_data['Duration'])
+    filtered_data = df[df["Profile Name"].isin(selected_profiles)]
+    filtered_data["Duration"] = pd.to_timedelta(filtered_data["Duration"])
     return filtered_data
+
 
 def preprocess_titles(df):
     """
@@ -30,14 +32,22 @@ def preprocess_titles(df):
     """
 
     def preprocess_and_classify_title(title):
-        main_title = title.split(':')[0].strip()
-        content_type = 'TV Show' if 'Season' in title or 'Episode' in title or ':' in title else 'Movie'
+        if title.startswith("Marvel's"):
+            title = title.replace("Marvel's", "")
+
+        main_title = title.split(":")[0].strip()
+        content_type = (
+            "TV Show"
+            if "Season" in title or "Episode" in title or ":" in title
+            else "Movie"
+        )
         return main_title, content_type
 
-    titles_types = df['Title'].apply(preprocess_and_classify_title)
-    df['Title'] = titles_types.apply(lambda x: x[0])
-    df['Type'] = titles_types.apply(lambda x: x[1])
+    titles_types = df["Title"].apply(preprocess_and_classify_title)
+    df["Title"] = titles_types.apply(lambda x: x[0])
+    df["Type"] = titles_types.apply(lambda x: x[1])
     return df
+
 
 def clean_and_format_data(df):
     """
@@ -50,15 +60,18 @@ def clean_and_format_data(df):
         pandas.DataFrame: Cleaned and formatted DataFrame.
     """
 
-    df = df.drop(columns=['Attributes', 'Supplemental Video Type', 'Bookmark', 'Latest Bookmark'])
-    df['Duration'] = df['Duration'].dt.total_seconds() / 3600
-    df['Start Time'] = pd.to_datetime(df['Start Time'])
-    df['Hour'] = df['Start Time'].dt.hour
-    df['Day'] = df['Start Time'].dt.day
-    df['Month'] = df['Start Time'].dt.month
-    df['Year'] = df['Start Time'].dt.year
+    df = df.drop(
+        columns=["Attributes", "Supplemental Video Type", "Bookmark", "Latest Bookmark"]
+    )
+    df["Duration"] = df["Duration"].dt.total_seconds() / 3600
+    df["Start Time"] = pd.to_datetime(df["Start Time"])
+    df["Hour"] = df["Start Time"].dt.hour
+    df["Day"] = df["Start Time"].dt.day
+    df["Month"] = df["Start Time"].dt.month
+    df["Year"] = df["Start Time"].dt.year
     df = df.drop(columns="Start Time")
     return df
+
 
 def categorize_devices(df):
     """
@@ -72,43 +85,60 @@ def categorize_devices(df):
     """
 
     def categorize_device(device_name):
-        device_name = device_name.lower()  # Convert to lowercase for case-insensitive matching
+        device_name = (
+            device_name.lower()
+        )  # Convert to lowercase for case-insensitive matching
 
-        if 'iphone' in device_name or 'ipad' in device_name:
-            return 'Apple Mobile Devices'
-        elif 'chrome' in device_name or 'edge' in device_name or 'safari' in device_name:
-            return 'Web Browsers'
-        elif 'android tv' in device_name or 'smart tv' in device_name or 'tv' in device_name:
-            return 'Smart TVs'
-        elif 'streaming stick' in device_name or 'chromecast' in device_name or 'firetv' in device_name or 'apple tv' in device_name or 'roku' in device_name:
-            return 'Streaming Devices'
-        elif 'ps4' in device_name or 'ps3' in device_name:
-            return 'Game Consoles'
-        elif 'set top box' in device_name:
-            return 'Set Top Boxes'
+        if "iphone" in device_name or "ipad" in device_name:
+            return "Apple Mobile Devices"
+        elif (
+            "chrome" in device_name or "edge" in device_name or "safari" in device_name
+        ):
+            return "Web Browsers"
+        elif (
+            "android tv" in device_name
+            or "smart tv" in device_name
+            or "tv" in device_name
+        ):
+            return "Smart TVs"
+        elif (
+            "streaming stick" in device_name
+            or "chromecast" in device_name
+            or "firetv" in device_name
+            or "apple tv" in device_name
+            or "roku" in device_name
+        ):
+            return "Streaming Devices"
+        elif "ps4" in device_name or "ps3" in device_name:
+            return "Game Consoles"
+        elif "set top box" in device_name:
+            return "Set Top Boxes"
         else:
-            return 'Other'
+            return "Other"
 
-    df['Device Category'] = df['Device Type'].apply(categorize_device)
+    df["Device Category"] = df["Device Type"].apply(categorize_device)
     df.drop(columns="Device Type", inplace=True)  # Assuming you don't need it anymore
     return df
+
 
 # ------------------------------
 # Main Execution
 # ------------------------------
-DATA_PATH = "/Users/pranavsukumaran/Desktop/netflix/data/ViewingActivity.csv"
+DATA_PATH = (
+    "/Users/pranavsukumaran/Desktop/Personal_dev/netflix/data/ViewingActivity.csv"
+)
 SELECTED_PROFILES = ["Pranav", "Home", "Priya"]
 
 data = load_and_filter_data(DATA_PATH, SELECTED_PROFILES)
-data = preprocess_titles(data.loc[data['Duration'] >= pd.Timedelta(minutes=5)].copy())
+data = preprocess_titles(data.loc[data["Duration"] >= pd.Timedelta(minutes=5)].copy())
 data = clean_and_format_data(data)
 data = categorize_devices(data)
 
-print(data) 
+print(data)
 
-output_dir = "/Users/pranavsukumaran/Desktop/netflix/project/data/"
+output_dir = "/Users/pranavsukumaran/Desktop/Personal_dev/netflix/project/data/"
 output_filepath = os.path.join(output_dir, "processed_data.csv")
 
-data.to_csv(output_filepath, index=False)  
+data.to_csv(output_filepath, index=False)
 
 print(f"Data saved to {output_filepath}")
